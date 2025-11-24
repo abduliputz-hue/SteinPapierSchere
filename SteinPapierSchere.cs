da6cc5bca848fcd40b25a3cd5752fa031ce13926
abduliputz-hue <abduliputz@gmail.com>
2025-11-24 20:59:43 +0100

    Update SteinPapierSchere.cs
    v0.0.2

--- SteinPapierSchere.cs
+++ SteinPapierSchere.cs
@@ -37,6 +37,8 @@ class SteinPapierSchere
 ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
         Console.WriteLine("\n=====================================");
+        Console.WriteLine("\nCoded & Designed By KARUNJI STUDIO");
+        Console.WriteLine("\n=====================================");
         TypingEffect("\n\nPress any key to continue...", 30);
         Console.ResetColor();
         Console.ReadKey();
@@ -55,9 +57,7 @@ class SteinPapierSchere
         ⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏⠀
         ⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁
         ⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀
-        ⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀"
-
-;
+        ⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀";
 
         string Papier = @"
                          ⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀
@@ -74,28 +74,35 @@ class SteinPapierSchere
     ⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
 
-
         string Schere = @"
-
     ⠀⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀⠀
     ⠀⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀
     ⠀⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀
-    ⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀ ⠀⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-";
+    ⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄
+    ⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆
+    ⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃
+    ⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀ ⠀⡷
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠈⠙⠳⠦⠾⠃";
 
-        while (true)
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("How many rounds do you want to play? > ");
+        int rounds = Convert.ToInt32(Console.ReadLine());
+        Console.ResetColor();
+
+        int cpu_player_score = 0;
+        int user_player_score = 0;
+
+        for (int round = 1; round <= rounds; round++)
         {
-            List<String> cpu_player = new List<string> { Stein, Papier, Schere };
-            Random rng = new Random();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Round {round}/{rounds}");
+            Console.ResetColor();
 
-            int cpu_player_score = 0;
-            int user_player_score = 0;
+            List<string> cpu_player = new List<string> { Stein, Papier, Schere };
+            Random rng = new Random();
 
             int index = rng.Next(cpu_player.Count);
             string cpu_player_move = cpu_player[index];
@@ -107,92 +114,74 @@ class SteinPapierSchere
             Console.ForegroundColor = ConsoleColor.Green;
             TypingEffect("YOUR MOVE : ", 30);
             Console.ResetColor();
+
             string user_player = Console.ReadLine();
             Console.WriteLine("\n===========================================");
 
-            if (user_player.ToLower() == "stein")
+            if (user_player.ToLower() == "stein") user_player = Stein;
+            else if (user_player.ToLower() == "papier") user_player = Papier;
+            else if (user_player.ToLower() == "schere") user_player = Schere;
+            else
             {
-                user_player = Stein;
+                round--;
+                continue;
+            }
 
-            }
-            else if (user_player.ToLower() == "papier")
-            {
-                user_player = Papier;
-            }
-            else if (user_player.ToLower() == "schere")
-            {
-                user_player = Schere;
-            }
-            else if (Console.ReadKey().Key == ConsoleKey.Escape)
-            {
-                Start();
-            }
-            
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write(user_player);
-
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write(cpu_player_move);
-
             Console.ResetColor();
 
+            if (cpu_player_move == user_player)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\nDRAW");
+            }
+            else if (
+                (cpu_player_move == Stein && user_player == Papier) ||
+                (cpu_player_move == Papier && user_player == Schere) ||
+                (cpu_player_move == Schere && user_player == Stein)
+            )
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n\nYOU WIN");
+                user_player_score++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\nYOU LOST");
+                cpu_player_score++;
+            }
+            Console.ResetColor();
 
-            if (cpu_player_move == Stein && user_player == Stein)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\n\nDRAW");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Papier && user_player == Papier)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\n\nDRAW");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Schere && user_player == Schere)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\n\nDRAW");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Stein && user_player == Papier)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\n\nYOU WIN");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Stein && user_player == Schere)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\n\nYOU LOST");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Papier && user_player == Stein)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\n\nYOU LOST");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Papier && user_player == Schere)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\n\nYOU WIN");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Schere && user_player == Stein)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\n\nYOU WIN");
-                Console.ResetColor();
-            }
-            else if (cpu_player_move == Schere && user_player == Papier)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\n\nYOU LOST");
-                Console.ResetColor();
-            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nScore – You: {user_player_score} | CPU: {cpu_player_score}");
+            Console.ResetColor();
+
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadKey();
         }
 
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("GAME FINISHED\n");
+        Console.ResetColor();
+
+        Console.WriteLine($"Final Score:\nYou: {user_player_score}\nCPU: {cpu_player_score}");
+
+        if (user_player_score > cpu_player_score)
+            Console.WriteLine("You won the game!");
+        else if (user_player_score < cpu_player_score)
+            Console.WriteLine("The CPU won the game!");
+        else
+            Console.WriteLine("It's a draw!");
+
+        Console.WriteLine("\nPress any key to return to the menu.");
+        Console.ReadKey();
+        Console.Clear();
+        Start();
     }
 
     public static void Help()
@@ -203,7 +192,7 @@ class SteinPapierSchere
 
     public static void Start()
     {
-        
+
         string s = @"
 +-----------+
 | 1 - Start |
